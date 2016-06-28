90a6ef5dc7e47461eadcebcbe8d95c4fd7c544fa
RBH laptop <hayward@ualberta.ca>
2016-06-28 07:12:39 -0600

    11x11, day 2

--- trans/sgf2p.py
+++ trans/sgf2p.py
@@ -5,6 +5,9 @@ moves = 0
 for line in sys.stdin:
   content = line.split('[')
   for x in content:
+    if x[:4] == 'swap' or x[:4] == 'SWAP':
+      moves += 1
+      print 'S',
     if x[0].islower() and x[1].isdigit(): 
       moves += 1
       if x[2].isdigit(): print x[:3],
