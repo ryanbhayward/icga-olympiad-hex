9b2b870646965ff0e25c8d4bc46eea10ac845b5d
Ryan Hayward <hayward@ualberta.ca>
2017-08-08 17:07:43 -0600

    continuations for mohex-ezo 13x13

--- trans/pix/sgf2hd.sh
+++ trans/pix/sgf2hd.sh
@@ -1,2 +1,3 @@
-for f in 11*.sgf; do ../sgf2p.py < $f | ../p2hd.11 > "${f%.sgf}.hexdiag" ; done 
-for f in 13*.sgf; do ../sgf2p.py < $f | ../p2hd.13 > "${f%.sgf}.hexdiag" ; done 
+#for f in 11*.sgf; do ../sgf2p.py < $f | ../p2hd.11 > "${f%.sgf}.hexdiag" ; done 
+for f in me*.sgf; do ../sgf2p.py < $f | ../p2hd.13 > "${f%.sgf}.hexdiag" ; done 
+for f in em*.sgf; do ../sgf2p.py < $f | ../p2hd.13 > "${f%.sgf}.hexdiag" ; done 
