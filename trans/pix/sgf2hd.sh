ec64fcb95ea4367b8ce9b2acf79b14e5fb7744bd
Ryan Hayward <hayward@.ualberta.ca>
2018-07-13 15:53:46 +0800

    first draft 2018 rpt

--- trans/pix/sgf2hd.sh
+++ trans/pix/sgf2hd.sh
@@ -1,3 +1,3 @@
-#for f in 11*.sgf; do ../sgf2p.py < $f | ../p2hd.11 > "${f%.sgf}.hexdiag" ; done 
-for f in me*.sgf; do ../sgf2p.py < $f | ../p2hd.13 > "${f%.sgf}.hexdiag" ; done 
-for f in em*.sgf; do ../sgf2p.py < $f | ../p2hd.13 > "${f%.sgf}.hexdiag" ; done 
+for f in *.sgf; do ../sgf2p.py < $f | ../p2hd.11 > "${f%.sgf}.hexdiag" ; done 
+# for f in me*.sgf; do ../sgf2p.py < $f | ../p2hd.13 > "${f%.sgf}.hexdiag" ; done 
+# for f in em*.sgf; do ../sgf2p.py < $f | ../p2hd.13 > "${f%.sgf}.hexdiag" ; done 
